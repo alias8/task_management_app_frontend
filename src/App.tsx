c691c244e0afcb033a2e7c18fb17de8b75b4ff5d
James Kirk <jameskirk@gmail.com>
2026-01-09 16:29:56 -0500

    Fix CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Navigate, Route, Routes, } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { PublicRoute } from './components/PublicRoute';
@@ -18,13 +23,20 @@ function App() {
             <h1
               style={{
                 textAlign: 'center',
-                margin: '50px 0 100px',
+                margin: '50px 0 50px',
               }}
             >
               Task Management App example
             </h1>
           </div>
-          <div style={{ height: '700px', maxWidth: '1200px', margin: '0 auto', padding: '0 20px' }}>
+          <div
+            style={{
+              height: '500px',
+              maxWidth: '1200px',
+              margin: '0 auto',
+              padding: '0 20px',
+            }}
+          >
             <Routes>
               <Route
                 path="/login"
